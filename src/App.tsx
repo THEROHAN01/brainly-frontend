6a163429ff40d6687e98371640d702447602c628
Rohan <rohansalunkhe700@gmail.com>
2025-11-01 19:06:12 +0000

    tried adding a icon to the button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 
 import './App.css'
 import { Button } from './components/ui/Button'
+import { PlusIcon } from './icons/PlusIcon'
 
 function App() {
 
   return (
     <>
-        <Button variant='primary' text='Share' />
-        <Button variant='secondary' text='Add Content' />
+        <Button startIcon={<PlusIcon size="sm" />} size="sm" variant='primary' text='Share'  />
+        <Button size='md' variant='secondary' text='Add Content' />
+        <Button size='lg' variant='secondary' text='Add Content' />
 
     </>
   )
 }
 
 export default App
+  
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -11,10 +11,24 @@ interface ButtonProps {
 }
 
 const variantStyles = {
-    "primary": "bg-purple-600 text-white",
-    "secondary":"bg-purple-400 text-purple-600"
-}
+    primary: "bg-purple-600 text-white ",
+    secondary: "bg-purple-200 text-purple-600 "
+};
 
-export const Button = (props: ButtonProps) => {
-    return <button onClick={props.onClick} className={variantStyles[props.variant]}>{props.text}</button>
-}
+const sizeStyles = {
+    sm: "px-2 py-1 text-sm",
+    md: "px-4 py-2 text-base",
+    lg: "px-6 py-4 text-lg"
+};
+
+export const Button = ({ variant, size = 'md', text, startIcon, endIcon, onClick }: ButtonProps) => {
+    const buttonClasses = `rounded-md flex font-semibold ${variantStyles[variant]} ${sizeStyles[size]}`;
+
+    return (
+        <button onClick={onClick} className={buttonClasses}>
+            {startIcon}
+            {text}
+            {endIcon}
+        </button>
+    );
+};
