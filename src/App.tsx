dce9a1d870adb8ae3fffbcf390d053e4c3a1451c
Rohan <rohansalunkhe700@gmail.com>
2025-12-21 07:20:13 +0000

    feat(ui): add all the required routes in the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Dashboard } from "./pages/dashboard";
 import { Signup } from "./pages/Signup";
-
-
+import { Signin } from "./pages/Signin";
+import { BrowserRouter, Routes , Route } from "react-router-dom";
 function App() {
 
 
   return (
-    <>  <Signup />
-        {/* <Dashboard /> */}
+    <>  
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+
+
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
